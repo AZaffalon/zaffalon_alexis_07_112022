d820e4af32aff46ea131c31e270598355f4b0553
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-12-01 18:15:34 +0100

    Finished About page (desktop & mobile

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,23 +3,25 @@ import '../assets/styles/dropdown.css'
 import arrow from'../assets/images/arrow.svg'
 
 
-function Dropdown() {
+function Dropdown({titleValue, dropContent}) {
   const [isOpen, setIsOpen] = useState(false)
 
   return isOpen ? (
     <div className="collapse">
       <div className="collapse--header" onClick={(e) => setIsOpen(false)}>
-        Fiabilité
+        {titleValue}
         <img src = {arrow} alt="arrow" className="arrow-up" />
       </div>
       <div className="collapse--expanded">
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
+        <p>
+          {dropContent}
+        </p>
       </div>
     </div>
   ) : (
     <div className="collapse">
       <div className="collapse--header" onClick={(e) => setIsOpen(true)}>
-        Fiabilité
+        {titleValue}
         <img src = {arrow} alt="arrow" />
       </div>
     </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,10 +10,10 @@ function About() {
       </div>
 
       <main className="dropdown-list">
-        <Dropdown />
-        <Dropdown />
-        <Dropdown />
-        <Dropdown />
+        <Dropdown titleValue={"Fiabilité"} dropContent={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."}/>
+        <Dropdown titleValue={"Respect"} dropContent={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."}/>
+        <Dropdown titleValue={"Service"} dropContent={"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."}/>
+        <Dropdown titleValue={"Sécurité"} dropContent={"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."}/>
       </main>
     </div>
   )
