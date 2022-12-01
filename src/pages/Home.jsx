fa6a84d7a011c05ae3c73ba19cee3ff4a307b976
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-12-01 10:36:04 +0100

    Finished Home page (desktop & mobile)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 import '../assets/styles/home.css'
 import LocationCard from "../components/LocationCard"
 
 function Home() {
+  const [locations, setLocations] = useState(null) 
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    fetch("./datas.json")
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        setLocations(data)
+      })
+      .catch(error => {
+        console.error("Problem fetching data:", error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return "Loading..."
+  }
+
   return (
     <main className="home">
       <div className="bckg-img">
@@ -10,12 +37,16 @@ function Home() {
         <div className="slogan">Chez vous, partout et ailleurs</div>
       </div>
       <section className="locations-list background--grey">
-        <LocationCard />
-        <LocationCard />
-        <LocationCard />
-        <LocationCard />
-        <LocationCard />
-        <LocationCard />
+        { locations.map(location => {
+            return (
+              <Link to={`/fiche-logement/${location.id}`} className="location-link">
+                {
+                  <LocationCard backgroundImage={location.cover} titleValue={location.title} key={location.id}/>
+                }
+              </Link>
+            )
+          }) 
+        }
       </section>
     </main>
   )
