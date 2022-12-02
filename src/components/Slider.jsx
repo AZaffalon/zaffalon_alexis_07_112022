788873f06e0ed9685546280c2211c913223315e4
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-12-02 09:42:10 +0100

    Create Slider and Apartement component and add dynamically datas + Front(not finished yet)

---
+++ src/components/Slider.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import '../assets/styles/slider.css'
+import arrow from'../assets/images/arrow.svg'
+
+export default function Slider({slides}) {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  return length === 1 ? (
+    <div className='carousel'>
+      <div className="slide-image">
+        <img src={slides} alt="" />
+      </div>
+    </div>
+  ) : (
+    <div className='carousel'>
+      {
+        slides.map((slide, index) => {
+          return (
+            <div key={index} className="slide-image">
+              <img src = {arrow} alt="arrow" className="arrow-left" onClick={() => prevSlide()} />
+              <img src = {arrow} alt="arrow" className="arrow-right" onClick={() => nextSlide()} />
+              {index === current && (
+                <img src={slide} alt="" />
+              )}
+            </div>
+          );
+        })
+      }
+    </div>
+  )
+}
--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -1,41 +1,92 @@
-import React from "react";
+// React
+import React, { useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+// Components
 import Tag from "../components/Tag";
 import Dropdown from "../components/Dropdown";
+import Slider from "../components/Slider";
+// Styles and images
 import '../assets/styles/apartment.css'
 import starGrey from '../assets/images/star-grey.svg'
 import starRed from '../assets/images/star-red.svg'
 
 function Apartment() {
+  const params = useParams()
+
+  const [apartment, setApartment] = useState(null) 
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    fetch("../datas.json")
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        }
+        throw response
+      })
+      .then(data => {
+        setApartment(data.find(a => a.id === params.id))
+        console.log(data.find(a => a.id === params.id))
+      })
+      .catch(error => {
+        console.error("Problem fetching data:", error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return "Loading..."
+  }
+
   return (
     <div className="apartment">
-      <div className="carousel"></div>
-      <h2>Paris center, on the romantic Canal Saint-Martin</h2>
-      <span>Paris, Île-de-France</span>
+      <Slider slides={apartment.pictures}/>
 
-      <div className="align-tags">
-        <Tag />
-        <Tag />
-        <Tag />
-      </div>
+      <div className="container">
+        <div className="wrapper column grow-1">
+          <h2>{apartment.title}</h2>
+          <span>{apartment.location}</span>
 
-      <div className="align">
-        <div className="star-container">
-          <img src= {starRed} alt="Star red" />
-          <img src= {starRed} alt="Star red" />
-          <img src= {starRed} alt="Star red" />
-          <img src= {starGrey} alt="Star grey" />
-          <img src= {starGrey} alt="Star grey" />
+          <div className="align-tags">
+            { apartment.tags.map(tag => {
+                return <Tag tagValue={tag} key={`${apartment.id}_${tag}`}/>
+              })
+            }
+          </div>
         </div>
 
-        <div className="name-picture">
-          <span>Alexandre Dumas</span>
-          <div className="picture"></div>
+        <div className="wrapper direction"> 
+          <div className="star-container grow-1">
+            {
+              [...Array(parseInt(apartment.rating))].map(() => {
+                return(
+                  <img src= {starRed} alt="Star red" />
+                )
+              })
+            }
+            {
+              [...Array(5 - parseInt(apartment.rating))].map(() => {
+                return(
+                  <img src= {starGrey} alt="Star grey" />
+                )
+              })
+            }
+          </div>
+
+          <div className="name-picture">
+            <span>{ apartment.host.name }</span>
+            <div className="picture" style={{
+              backgroundImage: `url(${apartment.host.picture})`
+            }}></div>
+          </div>
         </div>
       </div>
 
       <div className="align-dropdown">
-        <Dropdown />
-        <Dropdown />  
+        <Dropdown titleValue={"Description"} dropContent={apartment.description}/>
+        <Dropdown titleValue={"Équipements"} dropContent={apartment.equipments}/>  
       </div>
     </div>
   )
